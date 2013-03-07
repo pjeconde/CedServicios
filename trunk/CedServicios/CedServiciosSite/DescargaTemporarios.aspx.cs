b117a17e9b67006a94d5f7ed18f32daa9d54a1ee
pjeconde@gmail.com <pjeconde@gmail.com@a95d6c9f-360b-4085-5475-5bcfc0d173e5>
2013-03-07 13:25:57 +0000

    Descarga Temporarios

---
+++ trunk/CedServicios/CedServiciosSite/DescargaTemporarios.aspx.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+namespace CedServicios.Site
+{
+    public partial class DescargaTemporarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string filename = Request.QueryString.Get("archivo");
+            if (!String.IsNullOrEmpty(filename))
+            {
+                String dlDir = @"~/Temp/";
+                String path = Server.MapPath(dlDir + filename);
+                System.IO.FileInfo toDownload = new System.IO.FileInfo(path);
+                if (toDownload.Exists)
+                {
+                    Response.Clear();
+                    Response.AddHeader("Content-Disposition", "attachment; filename=" + toDownload.Name);
+                    Response.AddHeader("Content-Length", toDownload.Length.ToString());
+                    Response.ContentType = "application/octet-stream";
+                    Response.WriteFile(dlDir + filename);
+                    Response.End();
+                    toDownload.Delete();
+                }
+                else
+                {
+                     WebForms.Excepciones.Redireccionar(new EX.Validaciones.ArchivoInexistente(filename), "~/Excepcion.aspx");
+                }
+            }
+        }
+    }
+}
