884592a42f04aaa968553dee5f9b4c996d4fe2b5
pjeconde <pjeconde@gmail.com>
2018-07-27 23:22:34 -0300

    Entidad de Busquedad Laboral

---
+++ CedServicios/CedServiciosEntidades/BusquedaLaboral.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CedServicios.Entidades
+{
+    [Serializable]
+    public class BusquedaLaboral
+    {
+        private string email;
+        private string nombre;
+        private string nombreArchCV;
+        private BusquedaPerfil busquedaPerfil;
+	    private DateTime fechaAlta;
+	    private Boolean suscribe;
+	    private string comentario;
+        private string estado;
+
+        public BusquedaLaboral()
+        {
+            busquedaPerfil = new BusquedaPerfil();
+        }
+
+        public string Email
+        {
+            set
+            {
+                email = value;
+            }
+            get
+            {
+                return email;
+            }
+        }
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+        public string NombreArchCV
+        {
+            set
+            {
+                nombreArchCV = value;
+            }
+            get
+            {
+                return nombreArchCV;
+            }
+        }
+        public BusquedaPerfil BusquedaPerfil
+        {
+            set
+            {
+                busquedaPerfil = value;
+            }
+            get
+            {
+                return busquedaPerfil;
+            }
+        }
+        public string IdPerfilBusquedaLaboral
+        {
+            set
+            {
+                busquedaPerfil.IdBusquedaPerfil = value;
+            }
+            get
+            {
+                return busquedaPerfil.IdBusquedaPerfil;
+            }
+        }
+        public DateTime FechaAlta
+        {
+            set
+            {
+                fechaAlta = value;
+            }
+            get
+            {
+                return fechaAlta;
+            }
+        }
+        public Boolean Suscribe
+        {
+            set
+            {
+                suscribe = value;
+            }
+            get
+            {
+                return suscribe;
+            }
+        }
+        public string Comentario
+        {
+            set
+            {
+                comentario = value;
+            }
+            get
+            {
+                return comentario;
+            }
+        }
+        public string Estado
+        {
+            set
+            {
+                estado = value;
+            }
+            get
+            {
+                return estado;
+            }
+        }
+    }
+}
---
+++ CedServicios/CedServiciosEntidades/BusquedaPerfil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CedServicios.Entidades
+{
+    [Serializable]
+    public class BusquedaPerfil
+    {
+        private string idBusquedaPerfil;
+        private string descrBusquedaPerfil;
+        private string estado;
+
+        public string IdBusquedaPerfil
+        {
+            set
+            {
+                idBusquedaPerfil = value;
+            }
+            get
+            {
+                return idBusquedaPerfil;
+            }
+        }
+        public string DescrBusquedaPerfil
+        {
+            set
+            {
+                descrBusquedaPerfil = value;
+            }
+            get
+            {
+                return descrBusquedaPerfil;
+            }
+        }
+        public string Estado
+        {
+            set
+            {
+                estado = value;
+            }
+            get
+            {
+                return estado;
+            }
+        }
+    }
+}
