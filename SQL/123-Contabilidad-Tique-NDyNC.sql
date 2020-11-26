aa7a62b87ffc8870eb306e0a79715434f7b2bb48
pjeconde <condecopc@gmail.com>
2020-11-26 17:34:04 -0300

    Script Contab. Tique-ND y NC

---
+++ SQL/123-Contabilidad-Tique-NDyNC.sql
@@ -0,0 +1,51 @@
+insert EsquemaContable values (112, 'Compra', 'T', 'Pas-Db-Proveed', 1)
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'A', 'Gs-Compras', -Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'D', 'Gs-Compras', Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-21', 'Act-Cr-I-Iva21', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-10.5', 'Act-Cr-I-Iva10.5', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-27', 'Act-Cr-I-Iva27', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-0', 'Act-Cr-I-Iva0', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-3', 'Act-Cr-I-Otros', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-5', 'Act-Cr-I-IngBrut', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-2', 'Act-Cr-I-ImpInt', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-6', 'Act-Cr-I-ImpMun', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-4', 'Act-Cr-I-PercepINac', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=112
+
+insert EsquemaContable values (114, 'Compra', 'T', 'Pas-Db-Proveed', 1)
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'A', 'Gs-Compras', -Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'D', 'Gs-Compras', Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-21', 'Act-Cr-I-Iva21', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-10.5', 'Act-Cr-I-Iva10.5', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-27', 'Act-Cr-I-Iva27', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-0', 'Act-Cr-I-Iva0', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-3', 'Act-Cr-I-Otros', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-5', 'Act-Cr-I-IngBrut', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-2', 'Act-Cr-I-ImpInt', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-6', 'Act-Cr-I-ImpMun', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-4', 'Act-Cr-I-PercepINac', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=114
+
+insert EsquemaContable values (115, 'Compra', 'T', 'Pas-Db-Proveed', -1)
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'A', 'Gs-Compras', -Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'D', 'Gs-Compras', Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-21', 'Act-Cr-I-Iva21', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-10.5', 'Act-Cr-I-Iva10.5', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-27', 'Act-Cr-I-Iva27', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-0', 'Act-Cr-I-Iva0', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-3', 'Act-Cr-I-Otros', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-5', 'Act-Cr-I-IngBrut', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-2', 'Act-Cr-I-ImpInt', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-6', 'Act-Cr-I-ImpMun', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-4', 'Act-Cr-I-PercepINac', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=115
+
+insert EsquemaContable values (117, 'Compra', 'T', 'Pas-Db-Proveed', -1)
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'A', 'Gs-Compras', -Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'D', 'Gs-Compras', Signo from EsquemaContable where IdNaturalezaComprobante like 'Compra%' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-21', 'Act-Cr-I-Iva21', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-10.5', 'Act-Cr-I-Iva10.5', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-27', 'Act-Cr-I-Iva27', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-1-0', 'Act-Cr-I-Iva0', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-3', 'Act-Cr-I-Otros', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-5', 'Act-Cr-I-IngBrut', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-2', 'Act-Cr-I-ImpInt', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-6', 'Act-Cr-I-ImpMun', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
+insert EsquemaContable select IdTipoComprobante, IdNaturalezaComprobante, 'I-4', 'Act-Cr-I-PercepINac', -Signo from EsquemaContable where IdNaturalezaComprobante = 'Compra' and Concepto='T' and IdTipoComprobante=117
